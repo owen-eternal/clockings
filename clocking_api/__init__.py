d1d6bdf17484b96ae1950d86954054a7351897a4
owen-eternal <poesylyrics@gmail.com>
2021-01-13 03:20:20 +0200

    added setting.py file

--- clocking_api/__init__.py
+++ clocking_api/__init__.py
@@ -7,7 +7,7 @@ def create_app(settings='clocking_api.settings.DevConfig'):
     app = Flask(__name__)
     app.config.from_object(settings)
 
-    from .colections.resources import clocking_bp
+    from .collections.resources import clocking_bp
     app.register_blueprint(clocking_bp)
 
     return app
